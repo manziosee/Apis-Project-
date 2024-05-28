0d57c82df04f63845cbf7c4f6111da700cb1f98e
manzi osee <85952735+manziosee@users.noreply.github.com>
2024-05-28 12:24:11 +0300

    update commit #3

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteUserById, getUsers } from '../db/users';
+import { deleteUserById, getUsers, getUserById } from '../db/users';
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
     try {
@@ -24,3 +24,29 @@ export const deleteUser = async (req: express.Request, res: express.Response) =>
             return res.status(500).json({ message: 'Internal server error' });
         }
 }
+
+export const updateUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const { id } = req.params;
+        const { username } = req.body;
+
+       if (!username){
+        return res.status(400).json({ message: 'Username is required' });       }
+
+        const user = await getUserById(id);
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        user.username = username;
+
+        await user.save();
+
+        return res.json(user);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
